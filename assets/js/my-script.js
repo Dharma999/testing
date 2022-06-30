a40c2078ace0e6cf4984b302cd6b95aa8f7a28b1
Dharma999 <chaudhary.dharma.333@gmail.com>
2022-06-30 19:26:31 +0545

    Changed

---
+++ assets/js/my-script.js
@@ -0,0 +1,7 @@
+function zoomIn(x) {
+    x.style.transform = "scale(1.3)";
+}
+
+function zoomOut(x) {
+    x.style.transform = "scale(1)";
+}
---
+++ myBlog/myjs.js
@@ -0,0 +1,17 @@
+function openNav(x) {
+    var x = document.getElementById("my-nav");
+    x.style.display = "block";
+}
+
+function closeNav(x) {
+    var x = document.getElementById("my-nav");
+    x.style.display = "none";
+}
+
+function border(x) {
+    var x = document.getElementById("link").style.color = "red";
+}
+
+function border1(x) {
+    var x = document.getElementById("link").style.color = "black";
+}
